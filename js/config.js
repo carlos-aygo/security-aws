32e8f341ce9d014a8e0e80b4a985f7cc1d4774f8
carlos.sanchez <carlos@sumerlabs.com>
2021-11-18 08:59:07 -0500

    hola

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_2HJyTlcFh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 's0n7i80l0fojvfgdjjv55nmgg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_troSbMDn5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2o84sgsil71rjvt7bqofuha06j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
